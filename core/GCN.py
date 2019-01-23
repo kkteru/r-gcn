e1dec834befe1de5705b0d2eac55cadaec4d8d7e
Komal Kumar <komal.teru@gmail.com>
2019-01-23 12:17:21 -0500

    Fixed bug

--- core/GCN.py
+++ core/GCN.py
@@ -11,14 +11,14 @@ class GCN(nn.Module):
         super(GCN, self).__init__()
         self.params = params
         self.n_layers = self.params.gcn_layers
-        self.ent_emb = nn.Parameter(torch.rand(self.params.total_ent, self.params.emb_dim), requires_grad=params.no_encoder)  # (N, d)
+        self.ent_emb = nn.Parameter(torch.rand(self.params.total_ent, self.params.emb_dim), requires_grad=self.params.no_encoder)  # (N, d)
         self.rel_trans = nn.Parameter(torch.rand(self.params.total_rel, self.params.emb_dim, self.params.emb_dim), requires_grad=True)  # (R + 1 x d x d); + 1 for the self loop
 
     def forward(self, adj_mat):
         '''
         adj_mat: (R + 1, N, N)
         '''
-        if not params.no_encoder:
+        if not self.params.no_encoder:
             emb_acc = torch.empty(self.params.total_rel, self.params.total_ent, self.params.emb_dim)  # (R + 1 X N X d)
             for l in range(self.n_layers):
                 # print('in gcn; layer %d' % l)
