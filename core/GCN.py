d6dcca62daca692ba3f156cfba3290449992ad53
Komal Kumar <komal.teru@gmail.com>
2019-01-24 21:19:33 -0500

    Fixed bug

--- core/GCN.py
+++ core/GCN.py
@@ -25,7 +25,7 @@ class GCN(nn.Module):
             emb_acc = torch.empty(self.params.total_rel, self.params.total_ent, self.params.emb_dim).to(device=self.params.device)  # (R + 1 X N X d)
             for l in range(self.n_layers):
                 for i, mat in enumerate(adj_mat):
-                    print(emb_acc[i].dtype, mat.dtype, emb.dtype)
+                    print(emb_acc[i].is_cuda, mat.is_cuda, emb.is_cuda)
                     emb_acc[i] = torch.matmul(mat, emb).to(device=self.params.device)
                 tmp = torch.matmul(self.rel_trans[l], emb_acc.transpose(1, 2)).transpose(1, 2)  # (R + 1 X N X d) Shoud be different weights for different layers?
                 emb = F.relu(torch.sum(tmp, dim=0))
