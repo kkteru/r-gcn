ee9fea76cfdc2bd054e2a207661c711ec304f028
Komal Kumar <komal.teru@gmail.com>
2019-02-21 11:09:22 -0500

    Using less RAM

--- core/GCNLayer.py
+++ core/GCNLayer.py
@@ -34,18 +34,22 @@ class GCNLayer(nn.Module):
         adj_mat_list: (R x |E| x |E|)
         '''
 
-        rel_weights = torch.einsum('rb, bio -> rio', (self.basis_coeff, self.basis_weights))  # (R x in_size x out_size)
+        # rel_weights = torch.einsum('rb, bio -> rio', (self.basis_coeff, self.basis_weights))  # (R x in_size x out_size)
 
         # Aggregation (no explicit separation of Concat step here since we are simply averaging over all)
-        emb_acc = torch.empty(self.n_rel, inp.shape[0], inp.shape[1]).to(device=self.params.device)
+        # emb_acc = torch.empty(self.n_rel, inp.shape[0], inp.shape[1]).to(device=self.params.device)
+        out = torch.zeros(inp.shape[0], self.out_size)
         for i, mat in enumerate(adj_mat_list):
-            emb_acc[i] = torch.sparse.mm(mat, inp).to(device=self.params.device)
+            emb_acc = torch.sparse.mm(mat, inp).to(device=self.params.device)  # (|E| x inp_size)
+            rel_weight = torch.einsum('b, bio -> io', (self.basis_coeff[i], self.basis_weights))
+            out = out + torch.matmul(emb_acc, rel_weight)
+            if self.bias is not None:
+                out = out + self.bias[i].unsqueeze(0)  # (|E| x out_size)
+        # out_array = torch.matmul(emb_acc, rel_weight)  # (R x |E| x out_size)
 
-        out_array = torch.matmul(emb_acc, rel_weights)  # (R x |E| x out_size)
+        # if self.bias is not None:
+        #     out_array = out_array + self.bias.unsqueeze(1)
 
-        if self.bias is not None:
-            out_array = out_array + self.bias.unsqueeze(1)
-
-        out = torch.sum(out_array, dim=0)  # Could this aggregation over relations be done better?
+        # out = torch.sum(out_array, dim=0)  # Could this aggregation over relations be done better?
 
         return out  # (|E| x out_size)
